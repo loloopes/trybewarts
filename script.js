1862ad36df440101019f07360bbcb2b7825bd4cb
Lopes <gus.lcampos38@gmail.com>
2021-06-16 16:05:06 -0300

    Header concluido, ate o requisito 3 completo

---
+++ script.js
@@ -0,0 +1,14 @@
+function loginVerify() {
+  const User = 'tryber@teste.com';
+  const Pass = '123456';
+  const login = document.getElementById('Login').value;
+  const senha = document.getElementById('Senha').value;
+  if (User === login && Pass === senha) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
+
+const logar = document.getElementById('logar');
+logar.addEventListener('click', loginVerify);
